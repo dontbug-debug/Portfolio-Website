284936449ee648460584905b98b60720cb253238
dontbug-debug <60456289+dontbug-debug@users.noreply.github.com>
2023-12-26 13:23:22 -0600

    Adding timeline

--- minimal-portfolio/src/components/Timeline.jsx
+++ minimal-portfolio/src/components/Timeline.jsx
@@ -4,16 +4,17 @@ import TimelineItem from "./TimelineItem";
 
 function Timeline() {
   return (
-    <div className="flex flex-col md:flex-row justify-center my-20 w-full md:w-7/12">
-      {timeline.map((item) => (
-        <TimelineItem
-          key={item.id}
-          year={item.year}
-          title={item.title}
-          duration={item.duration}
-          details={item.details}
-        />
-      ))}
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        {timeline.map((item) => (
+          <TimelineItem
+            year={item.year}
+            title={item.title}
+            duration={item.duration}
+            details={item.details}
+          />
+        ))}
+      </div>
     </div>
   );
 }
