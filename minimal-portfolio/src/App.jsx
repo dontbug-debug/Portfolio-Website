fb8b9b0dda862a199dfc1c53e95439efdaa9e61c
dontbug-debug <60456289+dontbug-debug@users.noreply.github.com>
2024-01-01 19:34:29 -0600

    Add base URL and deploy script

--- minimal-portfolio/src/App.jsx
+++ minimal-portfolio/src/App.jsx
@@ -76,7 +76,13 @@ function App() {
           <Intro />
           <Portfolio />
           <Timeline />
-          <Contact />
+          {/* <Contact /> */}
+          {/* <button
+            type="button"
+            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
+          >
+            Resume
+          </button> */}
           <Footer />
         </div>
       </div>
