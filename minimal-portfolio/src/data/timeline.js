27b0740aedf8e61314d513956a4dc9e0e589c33c
Karthaveerya <60456289+dontbug-debug@users.noreply.github.com>
2025-01-21 11:46:34 -0700

    Update timeline.js
    deleted duration

--- minimal-portfolio/src/data/timeline.js
+++ minimal-portfolio/src/data/timeline.js
@@ -3,7 +3,6 @@ export default [
     key: 0,
     year: "2023",
     title: "Portfolio Website",
-    duration: "2 days",
     details:
       "I built this website to showcase my projects and skills that I worked on pursuing my degree.",
   },
@@ -11,7 +10,6 @@ export default [
     key: 3,
     year: "2023",
     title: "Long Covid Study Participant Portal",
-    duration: "12 weeks",
     details:
       "Built for an advanced software engineering course, a participant portal for a long covid study. Where I served as a build master for a team of 9 devs.",
   },
@@ -19,7 +17,6 @@ export default [
     key: 2,
     year: "2022",
     title: "Digi-Lib",
-    duration: "6 weeks",
     details:
       "Built for a software engineering course, a library management system. Worked with a team of 5 devs. I worked on the database and backend.",
   },
@@ -27,7 +24,6 @@ export default [
     key: 1,
     year: "2021",
     title: "K & S Shelter Portal",
-    duration: "10 days",
     details: "Built for a web development course, a basic CRUD web app.",
   },
 ];
